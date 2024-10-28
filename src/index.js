b53f8c7f9a9c8b1ccb87f95f8066f2926a845784
TriggerOnly <127595188+TriggerOnly@users.noreply.github.com>
2024-10-28 20:28:57 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from '../node_modules/react-router-dom/dist/index';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
