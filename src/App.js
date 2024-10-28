1bec8cdb706f6c66d3b517ace0a7637d6f32a290
Artem <artemmail@gmail.com>
2024-10-28 20:16:49 +0400

    del react-router

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Router>
+      {/* <Router>
         <Route/>
         <Route/>
-      </Router>
+      </Router> */}
     </div>
   );
 }
