8656c4026747f74eafd4b3aea545664d912dc362
TriggerOnly <127595188+TriggerOnly@users.noreply.github.com>
2024-10-29 20:21:09 +0400

    Update Square.js

--- src/components/Square/Square.js
+++ src/components/Square/Square.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import './Square.css'
+import './Square.css';
 import { useSelector } from 'react-redux';
 
-const Square = ({square}) => {
-    let buttonStyle = ' '
+const Square = ({ square }) => {
+    let blockStyle = '';
 
-    const snake = useSelector(store => store.game.snake)
-    const apple = useSelector(store => store.game.apple)
-    const {x, y} = square   
+    const snake = useSelector((store) => store.game.snake);
+    const apple = useSelector((store) => store.game.apple);
+    const { x, y } = square;
 
-    for(let s of snake) {
-        if(s.x === x && s.y === y) buttonStyle = 'snake'
-    }   
+    // Проверка для отображения змейки
+    for (let s of snake) {
+        if (s.x === x && s.y === y) {
+            blockStyle = 'snake';
+            break;
+        }
+    }
 
-    if(x === apple.x && y === apple.y) buttonStyle = 'apple'
+    // Проверка для отображения яблока
+    if (x === apple.x && y === apple.y) blockStyle = 'apple';
 
     return (
-        <span className='Square'>
-            <button className={buttonStyle}>
-                
-            </button>
-        </span>
+        <div className={`Square ${blockStyle}`}></div>
     );
 };
 
