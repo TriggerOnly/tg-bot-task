7f8c03e645efbc40f1eb263899767a816073e0a2
TriggerOnly <127595188+TriggerOnly@users.noreply.github.com>
2024-11-04 15:36:59 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css'
 import { useDispatch } from 'react-redux';
-import { saveKey } from './store/SnakeSlice';
+import { saveKey } from './store/SnakeSLice';
 import Board from './components/Board/Board';
 import Status from './components/Status'
 
