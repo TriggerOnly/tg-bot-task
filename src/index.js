fedbba08a4001d2e72bb4730145d696835797f1e
TriggerOnly <127595188+TriggerOnly@users.noreply.github.com>
2024-11-04 15:24:32 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,18 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { configureStore } from '@reduxjs/toolkit';
-import { gameReducer } from './store/gameSlice';
 import {Provider} from "react-redux"
-
-const store = configureStore({
-  reducer: {
-    game: gameReducer
-  }
-})
-import './App.css';
-import {configureStore} from '@reduxjs/toolkit';
 import {GameReducer} from './store/GameSlice';
-import {Provider} from 'react-redux';
 import { SnakeReducer } from './store/SnakeSLice';
 import { AppleReducer } from './store/AppleSlice';
 
@@ -31,9 +21,5 @@ root.render(
     <Provider store={store}>
       <App/>
     </Provider>      
-  </React.StrictMode>
-);
-      <App />
-    </Provider>
   </React.StrictMode>
 );
