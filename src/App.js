e1f40c8fcea87c31d5fc56168c64ec18c6ef1863
TriggerOnly <127595188+TriggerOnly@users.noreply.github.com>
2024-11-04 15:25:21 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import './App.css'
 import { useDispatch } from 'react-redux';
 import { saveKey } from './store/gameSlice';
 import { Board } from './components/Board/Board';
-import { useDispatch, useSelector } from 'react-redux';
-import { saveKey } from './store/SnakeSLice';
-import Board from './components/Board/Board';
 import Status from './components/Status'
-import './App.css'
 
 document.addEventListener(
   'touchmove',
